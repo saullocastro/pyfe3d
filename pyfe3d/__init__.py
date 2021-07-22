06a557d5fc11c0833bf300bfafecc8425998d043
saullocastro <saullogiovani@gmail.com>
2021-07-22 23:10:20 +0200

    ENH: added utils for coordinate systems

--- pyfe3d/__init__.py
+++ pyfe3d/__init__.py
@@ -4,9 +4,15 @@ Finite elements for 3D problems in Python/Cython
 
 """
 import numpy as np
+from alg3dpy.vector import asvector
+from alg3dpy.constants import Z, O
 
 from .quad4r import Quad4R, Quad4RData, Quad4RProbe
 from .beamc import BeamC, BeamCData, BeamCProbe
 DOF = 6
 INT = np.int64
 DOUBLE = np.float64
+
+from .coord import CoordR
+vecxz = asvector([1.,0.,1.])
+CSYSGLOBAL = CoordR(0, O, Z, vecxz)
