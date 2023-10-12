1c9e39977fdf2f11f9d97f6a5928a663010a7e2e
Saullo G. P. Castro <saullogiovani@gmail.com>
2023-10-12 09:03:27 +0200

    BUG: fixed casting of arrays in some systems + ENH: improved performance

--- pyfe3d/shellprop.pyx
+++ pyfe3d/shellprop.pyx
@@ -1007,6 +1007,6 @@ cdef class GradABDE:
         self.gradEij[2, 0] = (mat.u6 + (-1)*mat.u7*lp.xiE1 + 0*lp.xiE2)
 
         # d(E11 E12 E16 E22 E26 E66) / d(xiE1, xiE2)
-        self.gradEij[0, 1:] = h*np.array([mat.u7, 0])
-        self.gradEij[1, 1:] = h*np.array([0, -mat.u7])
-        self.gradEij[2, 1:] = h*np.array([-mat.u7, 0])
+        self.gradEij[0, 1] = h*mat.u7
+        self.gradEij[1, 2] = h*(-mat.u7)
+        self.gradEij[2, 1] = h*(-mat.u7)
