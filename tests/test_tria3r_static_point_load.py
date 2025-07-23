34c4195e1e5c6640f726a101abf00e5de015ec24
Saullo G. P. Castro <saullogiovani@gmail.com>
2025-07-23 11:19:28 +0200

    TST: internal force vector

--- tests/test_tria3r_static_point_load.py
+++ tests/test_tria3r_static_point_load.py
@@ -119,19 +119,16 @@ def test_tria3r_static_point_load(plot=False, refinement=1):
     bu = ~bk
 
     # point load at center node
-    f = np.zeros(N)
+    fext = np.zeros(N)
     fmid = 1.
     middle = np.isclose(x, a/2) & np.isclose(y, b/2)
-    f[2::DOF][middle] = fmid
+    fext[2::DOF][middle] = fmid
 
     KC0uu = KC0[bu, :][:, bu]
-    fu = f[bu]
-    assert fu.sum() == fmid
-
-    uu = spsolve(KC0uu, fu)
+    assert fext[bu].sum() == fmid
 
     u = np.zeros(N)
-    u[bu] = uu
+    u[bu] = spsolve(KC0uu, fext[bu])
 
     w = u[2::DOF].reshape(nx, ny).T
 
@@ -152,6 +149,17 @@ def test_tria3r_static_point_load(plot=False, refinement=1):
         plt.show()
 
     assert np.isclose(wmax_ref, w.max(), rtol=0.02)
+
+    fint = np.zeros(N)
+    for tria in trias:
+        tria.update_probe_xe(ncoords_flatten)
+        tria.update_probe_ue(u)
+        tria.update_fint(fint, prop)
+
+    # NOTE adding reaction forces to external force vector
+    Kku = KC0[bk, :][:, bu]
+    fext[bk] = Kku @ u[bu]
+    assert np.allclose(fint, fext)
 
 
 if __name__ == '__main__':
